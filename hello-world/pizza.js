fc526986bfe37a28ee30b447af6b9e36e3fa6c0d
laiducav <khoa20082001@gmail.com>
2022-06-07 10:46:30 +0700

    restructure & add typescript to learning

---
+++ hello-world/pizza.js
@@ -0,0 +1,4 @@
+// import howManyPizza from "how-many-pizza";
+
+const howManyPizza = require("how-many-pizza")
+console.log(howManyPizza(10000));
---
+++ hello-world/script.js
@@ -0,0 +1,21 @@
+if ("" == 0) {
+    // It is! But why??
+    console.log(true);
+}
+        
+
+    
+
+
+x = 10
+
+if (1 < x < 3) {
+    console.log(true);   
+}
+
+const obj = { width: 10, height: 15 };
+// Why is this NaN? Spelling is hard!
+const area = obj.width * obj.heigth;
+console.log(area);
+
+console.log(4 / []);
